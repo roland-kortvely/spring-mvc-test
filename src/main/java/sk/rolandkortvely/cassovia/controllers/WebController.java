65011263cbd3fda1048f80042f6854d398f0bcb3
Roland Körtvely <mikronum@gmail.com>
2019-06-08 14:21:49 +0200

    Users, Groups, PDF
    Took 1 hour 3 minutes

--- src/main/java/sk/rolandkortvely/cassovia/controllers/WebController.java
+++ src/main/java/sk/rolandkortvely/cassovia/controllers/WebController.java
@@ -1,5 +1,9 @@
 package sk.rolandkortvely.cassovia.controllers;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +18,12 @@ import sk.rolandkortvely.cassovia.models.User;
 import sk.rolandkortvely.cassovia.models.UserGroup;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Controller
 @Scope(WebApplicationContext.SCOPE_SESSION)
@@ -126,8 +136,6 @@ public class WebController extends AbstractController implements HandlerIntercep
             return;
         }
 
-        System.out.println("deleting " + data.getId());
-
         User user = User.find(sessionFactory, data.getId());
         if (user == null) {
             error("User not found!");
@@ -153,6 +161,16 @@ public class WebController extends AbstractController implements HandlerIntercep
             return;
         }
 
+        if (User.stream(sessionFactory)
+                .filter(u -> u.getUsername().equals(data.getUsername()))
+                .filter(u -> u.getId() != data.getId())
+                .findFirst().orElse(null) != null
+        ) {
+            error("Username already in use!");
+            response.sendRedirect(request.getContextPath() + "/admin/users");
+            return;
+        }
+
         User user;
 
         if (data.getId() != 0) {
@@ -166,7 +184,6 @@ public class WebController extends AbstractController implements HandlerIntercep
             flash("info", "User updated!");
         } else {
             user = new User(sessionFactory);
-            //data.save();
             flash("info", "User created!");
         }
 
@@ -180,23 +197,161 @@ public class WebController extends AbstractController implements HandlerIntercep
         response.sendRedirect(request.getContextPath() + "/admin/users");
     }
 
+    @RequestMapping("/admin/users/export")
+    public void users_export(HttpServletResponse response) throws Exception {
+        this.protectAdmin();
+
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("users.pdf"));
+
+        document.open();
+
+        PdfPTable table = new PdfPTable(4);
+
+        Stream.of("#", "username", "email", "role")
+                .forEach(columnTitle -> {
+                    PdfPCell header = new PdfPCell();
+                    header.setPhrase(new Phrase(columnTitle));
+                    table.addCell(header);
+                });
+        document.add(table);
+
+        List<User> list = User.all(sessionFactory);
+
+        list.forEach(user -> {
+
+            PdfPTable t = new PdfPTable(4);
+
+            PdfPCell id = new PdfPCell();
+            id.setPhrase(new Phrase(user.getId() + ""));
+            t.addCell(id);
+
+            PdfPCell username = new PdfPCell();
+            username.setPhrase(new Phrase(user.getUsername()));
+            t.addCell(username);
+
+            PdfPCell email = new PdfPCell();
+            email.setPhrase(new Phrase(user.getEmail()));
+            t.addCell(email);
+
+            PdfPCell group = new PdfPCell();
+            group.setPhrase(new Phrase(user.getRole().getGroupName()));
+            t.addCell(group);
+
+            t.completeRow();
+
+            try {
+                document.add(t);
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            }
+        });
+
+        document.close();
+
+        try {
+            InputStream is = new FileInputStream("users.pdf");
+            org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException ex) {
+            throw new RuntimeException("IOError writing file to output stream");
+        }
+    }
+
     @RequestMapping("/admin/groups")
     public String groups(Model model) {
 
         this.protectAdmin();
 
+        model.addAttribute("group", new UserGroup());
         model.addAttribute("groups", UserGroup.all(sessionFactory));
 
-        return "admin/groups";
+        return "admin/groups/index";
     }
 
-    @RequestMapping("/admin/groups/add")
-    public String group_add(Model model) {
-
+    @RequestMapping("/admin/groups/create")
+    public String groups_create(Model model) {
         this.protectAdmin();
 
+        model.addAttribute("user", new User());
         model.addAttribute("group", new UserGroup());
+        model.addAttribute("groups", UserGroup.all(sessionFactory));
 
-        return "admin/groups-add";
+        return "admin/groups/create";
+    }
+
+    @RequestMapping("/admin/groups/{id}")
+    public String groups_edit(HttpServletResponse response, Model model, @PathVariable Integer id) throws Exception {
+        this.protectAdmin();
+
+        UserGroup group = UserGroup.find(sessionFactory, id);
+        if (group == null) {
+            error("Group not found!");
+            response.sendRedirect(request.getContextPath() + "/admin/groups");
+            return "admin/groups/index";
+        }
+
+        model.addAttribute("user", new User());
+        model.addAttribute("group", group);
+        model.addAttribute("groups", UserGroup.all(sessionFactory));
+
+        return "admin/groups/create";
+    }
+
+    @PostMapping("/admin/groups/delete")
+    public void groups_delete(HttpServletResponse response, @ModelAttribute UserGroup data) throws Exception {
+        this.protectAdmin();
+
+        if (data.getId() == 0) {
+            error("Unknown group");
+            response.sendRedirect(request.getContextPath() + "/admin/groups");
+            return;
+        }
+
+        UserGroup group = UserGroup.find(sessionFactory, data.getId());
+        if (group == null) {
+            error("Group not found!");
+            response.sendRedirect(request.getContextPath() + "/admin/groups");
+            return;
+        }
+
+        if (group.getGroupName().equals("admin")) {
+            error("You cannot delete admin group!");
+            response.sendRedirect(request.getContextPath() + "/admin/groups");
+            return;
+        }
+
+        group.delete();
+
+        flash("info", "Group deleted!");
+
+        response.sendRedirect(request.getContextPath() + "/admin/groups");
+    }
+
+    @PostMapping("/admin/groups/store")
+    public void groups_store(HttpServletResponse response, @ModelAttribute UserGroup data) throws Exception {
+        this.protectAdmin();
+
+        UserGroup group;
+
+        if (data.getId() != 0) {
+            group = UserGroup.find(sessionFactory, data.getId());
+            if (group == null) {
+                error("Group not found!");
+                response.sendRedirect(request.getContextPath() + "/admin/groups");
+                return;
+            }
+
+            flash("info", "Group updated!");
+        } else {
+            group = new UserGroup(sessionFactory);
+            flash("info", "Group created!");
+        }
+
+        group.setGroupName(data.getGroupName());
+
+        group.save();
+
+        response.sendRedirect(request.getContextPath() + "/admin/groups");
     }
 }
